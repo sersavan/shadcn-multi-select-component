adf3a9cb173b7c264c8beed0dcf707c6c357e3f8
sersavan <sersavan@gmail.com>
2024-04-13 01:02:59 +0300

    v1.10

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 import "./globals.css";
 import { Toaster } from "@/components/ui/sonner";
@@ -39,6 +40,7 @@ export default function RootLayout({
           </div>
         </ThemeProvider>
         <Toaster position="bottom-center" />
+        <SpeedInsights />
       </body>
     </html>
   );
