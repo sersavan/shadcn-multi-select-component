f5706c30728efc6749da980d34fbbc7541f61f3e
Victorien <vcaquant@student.42.fr>
2024-07-24 12:33:36 +0200

    fix close button style

--- src/components/multi-select.tsx
+++ src/components/multi-select.tsx
@@ -360,7 +360,7 @@ export const MultiSelect = React.forwardRef<
                   <CommandSeparator />
                   <CommandItem
                     onSelect={() => setIsPopoverOpen(false)}
-                    className="flex-1 justify-center cursor-pointer"
+                    className="flex-1 justify-center cursor-pointer max-w-full"
                   >
                     Close
                   </CommandItem>
