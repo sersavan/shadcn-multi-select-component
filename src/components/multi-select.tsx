df3931fcc8c6bdac7e8490c90d734ee6196c7583
sersavan <sersavan@gmail.com>
2024-11-19 18:50:48 +0200

    Enhance multi-select component to allow pointer events on SVG elements

--- src/components/multi-select.tsx
+++ src/components/multi-select.tsx
@@ -200,7 +200,7 @@ export const MultiSelect = React.forwardRef<
             {...props}
             onClick={handleTogglePopover}
             className={cn(
-              "flex w-full p-1 rounded-md border min-h-10 h-auto items-center justify-between bg-inherit hover:bg-inherit",
+              "flex w-full p-1 rounded-md border min-h-10 h-auto items-center justify-between bg-inherit hover:bg-inherit [&_svg]:pointer-events-auto",
               className
             )}
           >
