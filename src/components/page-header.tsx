3c0d81be5216ba9d89b6e3203cd39b1f97e4a8cb
sersavan <sersavan@gmail.com>
2025-08-07 17:26:17 +0300

    refactor: Clean up formatting and indentation in page-header component

--- src/components/page-header.tsx
+++ src/components/page-header.tsx
@@ -3,66 +3,65 @@ import Balance from "react-wrap-balancer";
 import { cn } from "@/lib/utils";
 
 function PageHeader({
-  className,
-  children,
-  ...props
+	className,
+	children,
+	...props
 }: React.HTMLAttributes<HTMLDivElement>) {
-  return (
-    <section
-      className={cn(
-        "mx-auto flex max-w-[980px] flex-col items-center gap-2 py-4 mt-4",
-        className
-      )}
-      {...props}
-    >
-      {children}
-    </section>
-  );
+	return (
+		<section
+			className={cn(
+				"mx-auto flex max-w-[980px] flex-col items-center gap-2 py-4 mt-4",
+				className
+			)}
+			{...props}>
+			{children}
+		</section>
+	);
 }
 
 function PageHeaderHeading({
-  className,
-  ...props
+	className,
+	...props
 }: React.HTMLAttributes<HTMLHeadingElement>) {
-  return (
-    <h1
-      className={cn(
-        "text-center text-2xl font-bold leading-tight tracking-tighter md:text-3xl lg:leading-[1.1]",
-        className
-      )}
-      {...props}
-    />
-  );
+	return (
+		<h1
+			className={cn(
+				"text-center text-4xl font-bold leading-tight tracking-tighter md:text-5xl lg:leading-[1.1]",
+				className
+			)}
+			{...props}
+		/>
+	);
 }
 
 function PageHeaderDescription({
-  className,
-  ...props
+	className,
+	...props
 }: React.HTMLAttributes<HTMLParagraphElement>) {
-  return (
-    <Balance
-      className={cn(
-        "max-w-[750px] text-center text-sm text-muted-foreground sm:text-lg",
-        className
-      )}
-      {...props}
-    />
-  );
+	return (
+		<Balance
+			className={cn(
+				"max-w-[750px] text-center text-sm text-muted-foreground sm:text-lg",
+				className
+			)}
+			{...props}
+		/>
+	);
 }
 
 function PageActions({
-  className,
-  ...props
+	className,
+	...props
 }: React.HTMLAttributes<HTMLDivElement>) {
-  return (
-    <div
-      className={cn(
-        "flex w-full items-center justify-center space-x-4 py-4 md:pb-10",
-        className
-      )}
-      {...props}
-    />
-  );
+	return (
+		<div
+			className={cn(
+				"flex w-full items-center justify-center space-x-4 py-4 md:pb-10",
+				className
+			)}
+			{...props}
+		/>
+	);
 }
 
 export { PageHeader, PageHeaderHeading, PageHeaderDescription, PageActions };
